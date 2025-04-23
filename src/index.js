cc64a36e9da92e90eb64e8530b07a2e68d85b3b7
Nabeel Farooq <nabeel007dar@gmail.com>
2025-04-23 20:21:59 +0530

    Fix TypeError when user enters invalid location

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 (function displayWeather() {
   /*Variables declaration */
-  const containerForm = document.querySelector('[container-form]');
-  const userEntry = document.querySelector('[new-entry]');
-  const toggleSwitch = document.querySelector('[switch]');
-  const locButton = document.querySelector('[location-button]');
-  let input = 'New York';
+  const containerForm = document.querySelector("[container-form]");
+  const userEntry = document.querySelector("[new-entry]");
+  const toggleSwitch = document.querySelector("[switch]");
+  const locButton = document.querySelector("[location-button]");
+  let input = "New York";
   let searchTerm;
 
   /*Event Listeners for Location and user input */
-  locButton.addEventListener('click', (event) => {
+  locButton.addEventListener("click", (event) => {
     event.preventDefault();
     const successCallback = (location) => {
       let userLocation = `${
-        'lat=' + location.coords.latitude + '&lon=' + location.coords.longitude
+        "lat=" + location.coords.latitude + "&lon=" + location.coords.longitude
       }`;
       getWeather(userLocation);
     };
@@ -22,7 +22,7 @@
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
   });
 
-  containerForm.addEventListener('submit', (event) => {
+  containerForm.addEventListener("submit", (event) => {
     event.preventDefault();
     input = userEntry.value;
 
@@ -36,9 +36,16 @@
     try {
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?${searchTerm}&appid=49257f6591cfc3ed8daf0b5970d519cb&units=standard`,
-        { mode: 'cors' }
+        { mode: "cors" }
       );
       const data = await response.json();
+      // Check if the API returned an error code
+      if (data.cod && data.cod !== 200) {
+        throw new Error(
+          data.message ||
+            "Location not found. Please try again with a valid location."
+        );
+      }
       displayWeather(data);
     } catch (err) {
       alert(err);
@@ -47,37 +54,42 @@
 
   /*Render Weather function*/
   function displayWeather(data) {
+    // Verify that data contains the required properties before proceeding
+    if (!data || !data.weather || !data.weather[0]) {
+      alert("Invalid weather data received. Please try a different location.");
+      return;
+    }
     /* Set background depending on weather*/
     switch (data.weather[0].main) {
-      case 'Clear':
+      case "Clear":
         document.body.style.backgroundImage = 'url("clear.jpg")';
         break;
-      case 'Clouds':
+      case "Clouds":
         document.body.style.backgroundImage = 'url("cloudy.jpg")';
         break;
-      case 'Rain':
-      case 'Drizzle':
-      case 'Mist':
+      case "Rain":
+      case "Drizzle":
+      case "Mist":
         document.body.style.backgroundImage = 'url("rain.jpg")';
         break;
-      case 'Thunderstorm':
+      case "Thunderstorm":
         document.body.style.backgroundImage = 'url("thunderstorm.jpg")';
         break;
-      case 'Snow':
+      case "Snow":
         document.body.style.backgroundImage = 'url("snow.jpg")';
         break;
       default:
         break;
     }
 
-    const cityName = document.querySelector('[cityName]');
-    const cityTemperature = document.querySelector('[cityTemperature]');
-    const cityWeatherDescription = document.querySelector('[cityWeather]');
-    const tempFeeling = document.querySelector('[tempFeeling]');
-    const humidityMeter = document.querySelector('[humidity]');
-    const windMeter = document.querySelector('[wind]');
-    const weatherImg = document.getElementById('image');
-    const datePreview = document.querySelector('[date-time]');
+    const cityName = document.querySelector("[cityName]");
+    const cityTemperature = document.querySelector("[cityTemperature]");
+    const cityWeatherDescription = document.querySelector("[cityWeather]");
+    const tempFeeling = document.querySelector("[tempFeeling]");
+    const humidityMeter = document.querySelector("[humidity]");
+    const windMeter = document.querySelector("[wind]");
+    const weatherImg = document.getElementById("image");
+    const datePreview = document.querySelector("[date-time]");
     const dateAndTime = data.timezone;
     const weatherDescription = data.weather[0].description;
     let temp = data.main.temp;
@@ -89,11 +101,11 @@
     if (toggleSwitch.checked) {
       temp = kelvinToFahrenheit(temp);
       tempFeel = kelvinToFahrenheit(tempFeel);
-      cityTemperature.innerHTML = `${temp + '&degF'}`;
+      cityTemperature.innerHTML = `${temp + "&degF"}`;
     } else {
       temp = kelvinToCelcius(temp);
       tempFeel = kelvinToCelcius(tempFeel);
-      cityTemperature.innerHTML = `${temp + '&degC'}`;
+      cityTemperature.innerHTML = `${temp + "&degC"}`;
     }
 
     weatherImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
@@ -101,27 +113,27 @@
     cityWeatherDescription.innerHTML =
       weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
 
-    tempFeeling.innerHTML = `${'Feels like: '}${tempFeel + '&deg'}`;
+    tempFeeling.innerHTML = `${"Feels like: "}${tempFeel + "&deg"}`;
 
-    humidityMeter.innerHTML = `${'Humidity levels: '}${
-      data.main.humidity + '%'
+    humidityMeter.innerHTML = `${"Humidity levels: "}${
+      data.main.humidity + "%"
     }`;
-    windMeter.innerHTML = `${'Wind: '}${data.wind.speed + 'k/m'}`;
+    windMeter.innerHTML = `${"Wind: "}${data.wind.speed + "k/m"}`;
 
-    toggleSwitch.addEventListener('change', () => {
+    toggleSwitch.addEventListener("change", () => {
       if (toggleSwitch.checked) {
         setTimeout(() => {
           temp = toFahrenheit(temp);
           tempFeel = toFahrenheit(tempFeel);
-          cityTemperature.innerHTML = `${temp + '&degF'}`;
-          tempFeeling.innerHTML = `${'Feels like: '}${tempFeel + '&deg'}`;
+          cityTemperature.innerHTML = `${temp + "&degF"}`;
+          tempFeeling.innerHTML = `${"Feels like: "}${tempFeel + "&deg"}`;
         }, 150);
       } else {
         setTimeout(() => {
           temp = toCelsius(temp);
           tempFeel = toCelsius(tempFeel);
-          cityTemperature.innerHTML = `${temp + '&degC'}`;
-          tempFeeling.innerHTML = `${'Feels like: '}${tempFeel + '&deg'}`;
+          cityTemperature.innerHTML = `${temp + "&degC"}`;
+          tempFeeling.innerHTML = `${"Feels like: "}${tempFeel + "&deg"}`;
         }, 150);
       }
     });
@@ -154,15 +166,15 @@
     if (isNaN(input) && containsNumber(input)) {
       searchTerm = input;
     } else if (isANumber(input) && input.length <= 5) {
-      searchTerm = `${'zip='}${input}`;
+      searchTerm = `${"zip="}${input}`;
     } else {
-      searchTerm = `${'q='}${input}`;
+      searchTerm = `${"q="}${input}`;
     }
   }
 
   /* Clear user input function*/
   function clear() {
-    userEntry.value = '';
+    userEntry.value = "";
   }
 
   /* Is a number or doesn't contain a number functions */
